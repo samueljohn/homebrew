e457b25ac52cd8e94371e651a675304b8524b1cc
samueljohn <github@samueljohn.de>
2012-02-16 17:33:34 +0100

    libsvm: Now installing to pythonX.Y/site-packages via symlinks
    - This allows to brew remove them.
    - Further begun work on --qt option to install svm-toy (not yet finished)

--- Library/Formula/libsvm.rb
+++ Library/Formula/libsvm.rb
@@ -6,7 +6,10 @@ class Libsvm < Formula
   homepage 'http://www.csie.ntu.edu.tw/~cjlin/libsvm/'
 
   def options
-    [ "--python", "Install Python bindings."]
+    [
+      ["--python", "Install Python bindings."],
+      ["--qt", "Install qt4-based gui called svm-toy."]
+    ]
   end
 
   def install
@@ -20,17 +23,28 @@ class Libsvm < Formula
 
     if ARGV.include? "--python"
       Dir.chdir "python" do
+        ohai "Python bindings and tools..."
         system "make"
         # Python bindings are just two plain .py files:
+        mkdir_p "#{lib}/#{pythonxy}/site-packages/"
         cp( ["svm.py", "svmutil.py"],
-            "#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages/",
+            "#{lib}/#{pythonxy}/site-packages/",
             :verbose => true)
       end
       Dir.chdir "tools" do
-        mkdir_p "#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages/svmtools"
+        mkdir_p "#{lib}/#{pythonxy}/site-packages/svmtools"
         cp( ["checkdata.py", "easy.py", "grid.py", "subset.py"],
-            "#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages/svmtools",
+            "#{lib}/#{pythonxy}/site-packages/svmtools",
             :verbose => true )
+      end
+    end
+
+    if ARGV.include? "--qt"
+      Dir.chdir "svm-toy/qt" do
+        ENV.append("CFLAGS", "-L#{HOMEBREW_PREFIX}/lib/QtGui.framework/")
+        # todo lQtGui is not found because there is no libQtGui ... hmmm
+        system "make", "INCLUDE=#{HOMEBREW_PREFIX}/include", "MOC=moc"
+        bin.install "svm-toy"
       end
     end
 
@@ -39,22 +53,24 @@ class Libsvm < Formula
     include.install ['svm.h']
   end
 
-  if ARGV.include? "--python"
-    def caveats; <<-EOS.undent
-      For non-homebrew Python, you need to amend your PYTHONPATH like so:
-        export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages:$PYTHONPATH
-      Libsvm tools have bin installed to:
-        "#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages/svmtools"
-      EOS
-    end
+  def caveats
+    s = ""
+    python_caveats = <<-EOS.undent
+    For non-homebrew Python, you need to amend your PYTHONPATH like so:
+      export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{pythonxy}/site-packages:$PYTHONPATH
+    Libsvm tools have bin installed to:
+      "#{HOMEBREW_PREFIX}/lib/#{pythonxy}/site-packages/svmtools"
+    EOS
+    s += python_caveats if ARGV.include? "--python"
   end
 
   def test
-    system "make test"
+    # todo
   end
 end
 
 
-def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+def pythonxy
+  # Something like "python2.7"
+  "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
 end
